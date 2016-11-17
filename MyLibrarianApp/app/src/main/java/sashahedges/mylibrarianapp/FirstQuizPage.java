db83e0235bce7ba8fda4e13d8de9a22f42ab5cc9
llamallove12 <avhedges@crimson.ua.edu>
2016-11-16 22:14:51 -0600

    added the add functionality

--- MyLibrarianApp/app/src/main/java/sashahedges/mylibrarianapp/FirstQuizPage.java
+++ MyLibrarianApp/app/src/main/java/sashahedges/mylibrarianapp/FirstQuizPage.java
@@ -1,7 +1,10 @@
 package sashahedges.mylibrarianapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstQuizPage extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class FirstQuizPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_quiz_page);
+
+        final Button doIt = (Button) findViewById(R.id.NextPageButton);
+        doIt.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        Intent nextScreen = new Intent(v.getContext(),SecondQuizPage.class);
+                        startActivity(nextScreen);
+
+
+                    }
+                }
+
+
+        );
+
     }
 }
--- MyLibrarianApp/app/src/main/java/sashahedges/mylibrarianapp/SecondQuizPage.java
+++ MyLibrarianApp/app/src/main/java/sashahedges/mylibrarianapp/SecondQuizPage.java
@@ -1,7 +1,15 @@
 package sashahedges.mylibrarianapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondQuizPage extends AppCompatActivity {
 
@@ -9,5 +17,30 @@ public class SecondQuizPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_quiz_page);
+        final Button addButton = (Button) findViewById(R.id.AddButton);
+        final EditText nameBook = (EditText) findViewById(R.id.nameBook);
+
+
+        addButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        TextView bookList = (TextView) findViewById(R.id.bookList);
+                        bookList.setMovementMethod(new ScrollingMovementMethod());
+                        String book = nameBook.getText().toString();
+                        String bookListString = bookList.getText().toString();
+                        bookList.setText(bookListString+"\n"+book);
+                        nameBook.setText("");
+                        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                        mgr.hideSoftInputFromWindow(nameBook.getWindowToken(),0);
+
+                    }
+                }
+
+
+
+        );
+
+
     }
 }
