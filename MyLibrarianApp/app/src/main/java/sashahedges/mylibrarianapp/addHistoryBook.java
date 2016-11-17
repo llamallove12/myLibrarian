1578c2db9794544a005b180d95b4473e205d8c19
cpnoe <cpnoe@crimson.ua.edu>
2016-11-16 20:36:39 -0600

    hi

---
+++ MyLibrarianApp/app/src/main/java/sashahedges/mylibrarianapp/addHistoryBook.java
@@ -0,0 +1,13 @@
+package sashahedges.mylibrarianapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addHistoryBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_history_book);
+    }
+}
