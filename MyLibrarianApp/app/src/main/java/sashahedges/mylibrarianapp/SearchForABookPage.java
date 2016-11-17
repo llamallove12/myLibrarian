59de292c577882e1cde22accd2be5a28f050c36f
Jessie Melton <jrmelton@crimson.ua.edu>
2016-11-16 20:23:11 -0600

    committing..

---
+++ MyLibrarianApp/app/src/main/java/sashahedges/mylibrarianapp/SearchForABookPage.java
@@ -0,0 +1,13 @@
+package sashahedges.mylibrarianapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchForABookPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_for_abook_page);
+    }
+}
