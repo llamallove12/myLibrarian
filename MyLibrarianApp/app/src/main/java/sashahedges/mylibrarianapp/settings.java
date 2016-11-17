9113aa0fc2636231a8797fc986acd0c783ea481c
cpnoe <cpnoe@crimson.ua.edu>
2016-11-16 21:12:35 -0600

    hi

--- MyLibrarianApp/app/src/main/java/sashahedges/mylibrarianapp/settings.java
+++ MyLibrarianApp/app/src/main/java/sashahedges/mylibrarianapp/settings.java
@@ -1,7 +1,11 @@
 package sashahedges.mylibrarianapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class settings extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        final Button btnUserProfile = (Button) findViewById(R.id.goUserProfileButton);
+        btnUserProfile.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            Intent nextScreen = new Intent(v.getContext(),quiz.class);
+            startActivity(nextScreen);
+        }
+        });
     }
+
+
+
+
 }
