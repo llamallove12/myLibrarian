c23914e69e264b6b7db606a00e9649e816796308
Jessie Melton <jrmelton@crimson.ua.edu>
2016-11-16 18:59:50 -0600

    Added First Quiz Page

---
+++ MyLibrarianApp/app/src/main/java/sashahedges/mylibrarianapp/FirstQuizPage.java
@@ -0,0 +1,13 @@
+package sashahedges.mylibrarianapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstQuizPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_quiz_page);
+    }
+}
