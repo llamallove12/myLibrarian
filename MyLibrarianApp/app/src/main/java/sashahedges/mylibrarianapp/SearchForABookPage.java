33c1469dbf67f37d194bc6cad325276006d38151
Jessie Melton <jrmelton@crimson.ua.edu>
2016-11-17 09:10:09 -0600

    committing...

--- MyLibrarianApp/app/src/main/java/sashahedges/mylibrarianapp/SearchForABookPage.java
+++ MyLibrarianApp/app/src/main/java/sashahedges/mylibrarianapp/SearchForABookPage.java
@@ -1,7 +1,12 @@
 package sashahedges.mylibrarianapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class SearchForABookPage extends AppCompatActivity {
 
@@ -9,5 +14,41 @@ public class SearchForABookPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_for_abook_page);
+
+        final RadioButton TitleRB = (RadioButton) findViewById(R.id.TitleRadioButton);
+        TitleRB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final TextView TitleTB = (TextView) findViewById(R.id.TitleTextBox);
+                TitleTB.setText(getting.getTitle());
+            }
+        });
+
+
+        final RadioButton AuthorRB = (RadioButton) findViewById(R.id.AuthorRadioButton);
+        TitleRB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final TextView AutorTB = (TextView) findViewById(R.id.AuthorTextBox);
+                AuthorTB.setText(getting.getAuthor());
+            }
+        });
+
+        final RadioButton ISBNRB = (RadioButton) findViewById(R.id.ISBNRadioButton);
+        ISBNRB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final TextView ISBNTB = (TextView) findViewById(R.id.ISBNTextBox);
+                ISBNTB.setText(getting.getISBN());
+            }
+        });
+
+
+
+
+
     }
 }
